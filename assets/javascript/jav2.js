8e6361745343229cc0f8d192e2d3cf959a5919ab
RBG <dschiavo.262@gmail.com>
2018-10-27 18:06:50 -0400

    final files

--- assets/javascript/jav2.js
+++ assets/javascript/jav2.js
@@ -1,5 +1,6 @@
 var animals = ["squirrel", "horse", "dog", "cat", "mouse", "lizard", "snake", "tiger", "monkey", "elephant", "whale"];
 
+
 // displayAnimalGif function re-renders the HTML to display the appropriate content
 function displayAnimalGif() {
 
@@ -30,19 +31,35 @@ function displayAnimalGif() {
       // Creating a paragraph tag with the result item's rating
       var p = $("<p>").text("Rating: " + results[i].rating);
 
+
       // Creating and storing an image tag
       var animalImage = $("<img>");
       // Setting the src attribute of the image to a property pulled off the result item
-      animalImage.attr("src", results[i].images.original_still.url);
+
+      animalImage.attr("src", results[i].images.fixed_width_still.url);
+
+
 
       // Appending the paragraph and image tag to the animalDiv
       animalDiv.append(p);
       animalDiv.append(animalImage);
+      console.log(animalImage);
+
+      //change the still gif to a moving gif when clicked
+
+      $("animalImage").on("click", function () {
+        src = animalImage.attr("src");
+        if ($(src.split("_")).last()[0] == "still.gif")
+          animalImage.attr('src', src.replace('_still.gif', '.gif'));
+        else
+          animalImage.attr('src', src.replace('.gif', '_still.gif'));
+
+      });
+
+
 
       // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
       $("#gifs-appear-here").prepend(animalDiv);
-
-
 
 
     }
@@ -50,42 +67,41 @@ function displayAnimalGif() {
   });
 }
 
-  // Function for displaying animal data
-  function renderButtons() {
+// Function for displaying animal data
+function renderButtons() {
 
-    // Deleting the movies prior to adding new movies
-    // (this is necessary otherwise you will have repeat buttons)
-    $("#buttons-view").empty();
+  // Deleting the movies prior to adding new movies
+  // (this is necessary otherwise you will have repeat buttons)
+  $("#buttons-view").empty();
 
-    // Looping through the array of movies
-    for (var i = 0; i < animals.length; i++) {
+  // Looping through the array of movies
+  for (var i = 0; i < animals.length; i++) {
 
-      // Then dynamicaly generating buttons for each movie in the array
-      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-      var a = $("<button>");
-      // Adding a class of movie-btn to our button
-      a.addClass("animal-btn");
-      // Adding a data-attribute
-      a.attr("data-name", animals[i]);
-      // Providing the initial button text
-      a.text(animals[i]);
-      // Adding the button to the buttons-view div
-      $("#buttons-view").append(a);
-    }
+
+    var a = $("<button>");
+
+    a.addClass("animal-btn");
+
+    a.attr("data-name", animals[i]);
+
+    a.text(animals[i]);
+
+    $("#buttons-view").append(a);
   }
+}
 
-  // This function handles events where an animal button is clicked
-  $("#add-animal").on("click", function (event) {
-    event.preventDefault();
-    // This line grabs the input from the textbox
-    var animal = $("#animal-input").val().trim();
+// This function handles events where an animal button is clicked
+$("#add-animal").on("click", function (event) {
+  event.preventDefault();
+  // This line grabs the input from the textbox
+  var animal = $("#animal-input").val().trim();
 
-    // Adding animal from the textbox to our array
-    animals.push(animal);
+  // Adding animal from the textbox to our array
+  animals.push(animal);
 
-    // Calling renderButtons which handles the processing of our animal array
-    renderButtons();
-  });
+  // Calling renderButtons which handles the processing of our animal array
+  renderButtons();
+});
 
 
 
